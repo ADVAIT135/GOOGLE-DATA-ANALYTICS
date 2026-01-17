d16fde2c2a3ff7f51380a75111c87beda1c0bfb4
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2026-01-17 11:02:12 +0530

    Module 2 Exercise

---
+++ 7. Data Analysis with R Programming/Exercise_2.R
@@ -0,0 +1,17 @@
+data("ToothGrowth")
+view(ToothGrowth)
+
+filtered_tg <- filter(ToothGrowth, dose == 0.5)
+
+arrange(filtered_tg, len)
+
+arrange(filter(ToothGrowth, dose == 0.5), len)
+
+filtered_toothgrowth <- ToothGrowth %>% 
+  filter(dose == 0.5) %>% 
+  arrange(len)
+
+filtered_toothgrowth_1 <- ToothGrowth %>%
+  filter(dose == 0.5) %>% 
+  group_by(supp) %>% 
+  summarise(mean_len = mean(len, na.rm = T), .group = "drop")
